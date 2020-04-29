83306ea23c1e94d5fff4e631edd93f39a5f354cd
chlee1001 <chlee1001@naver.com>
2020-04-29 16:07:43 +0900

    Test ImagePicker Function_200429

--- hwajilguji/app/src/main/java/com/chlee/hwajilguji/GalleryActivity.java
+++ hwajilguji/app/src/main/java/com/chlee/hwajilguji/GalleryActivity.java
@@ -4,53 +4,53 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 import com.zhihu.matisse.Matisse;
 import com.zhihu.matisse.MimeType;
 import com.zhihu.matisse.engine.impl.GlideEngine;
-import com.zhihu.matisse.engine.impl.PicassoEngine;
-import com.zhihu.matisse.filter.Filter;
 import com.zhihu.matisse.internal.entity.CaptureStrategy;
 
-import java.util.List;
 
 public class GalleryActivity extends AppCompatActivity{
-
     private static final int REQUEST_CODE_CHOOSE = 23;
+    private static ImageView gallery_image;
 
     @SuppressLint("CheckResult")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
-//        findViewById(R.id.dracula).setOnClickListener(this);
-        RxPermissions rxPermissions = new RxPermissions(this);
-        rxPermissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                .subscribe(permission -> {
-                    if (permission) {
-                        startAction();
-                    } else {
-                        Toast.makeText(GalleryActivity.this, R.string.permission_request_denied, Toast.LENGTH_LONG);
-                     }
-                });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint({"CheckResult", "ShowToast"})
+            @Override
+            public void onClick(View view) {
+                RxPermissions rxPermissions = new RxPermissions(GalleryActivity.this);
+                rxPermissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .subscribe(permission -> {
+                            if (permission) {
+                                ImagePicker();
+                            } else {
+                                Toast.makeText(GalleryActivity.this, R.string.permission_request_denied, Toast.LENGTH_LONG);
+                            }
+                        });
+            }
+        });
 
+        gallery_image = (ImageView) findViewById(R.id.gallery_imageview);
     }
 
-
-    private void startAction() {
+    private void ImagePicker() {
         Matisse.from(GalleryActivity.this)
                 .choose(MimeType.ofImage(), false)
                 .theme(R.style.Matisse_Dracula)
@@ -75,17 +75,28 @@ public class GalleryActivity extends AppCompatActivity{
                     Log.e("isChecked", "onCheck: isChecked=" + isChecked);
                 })
                 .forResult(REQUEST_CODE_CHOOSE);
+
     }
 
-    List<Uri> mSelected;
+    @SuppressLint("CheckResult")
     @Override //Receive the returned address
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == REQUEST_CODE_CHOOSE && resultCode == RESULT_OK) {
-            mSelected = Matisse.obtainResult(data);
-            Log.d("Matisse", "mSelected: " + mSelected);
+            String path = Matisse.obtainPathResult(data).get(0);
+            if (path != null) {
+                Glide.with(this)
+                        .asBitmap() // some .jpeg files are actually gif
+                        .load(path)
+                        .apply(new RequestOptions() { {
+                            override(Target.SIZE_ORIGINAL);
+                        }})
+                        .into(gallery_image);
+            } else
+                Toast.makeText(this, "Uri is null", Toast.LENGTH_SHORT).show();
         }
 
     }
-
 }
+
--- hwajilguji/app/src/main/java/com/chlee/hwajilguji/MainActivity.java
+++ hwajilguji/app/src/main/java/com/chlee/hwajilguji/MainActivity.java
@@ -6,25 +6,28 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+
 import com.tbruyelle.rxpermissions2.RxPermissions;
 import com.zhihu.matisse.Matisse;
 import com.zhihu.matisse.MimeType;
 import com.zhihu.matisse.engine.impl.GlideEngine;
 import com.zhihu.matisse.internal.entity.CaptureStrategy;
 
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE_CHOOSE = 23;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,61 +35,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button galleryBtn = findViewById(R.id.galleryBtn);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint({"CheckResult", "ShowToast"})
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                RxPermissions rxPermissions = new RxPermissions(MainActivity.this);
-                rxPermissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        .subscribe(permission -> {
-                            if (permission) {
-                                ImagePicker();
-                            } else {
-                                Toast.makeText(MainActivity.this, R.string.permission_request_denied, Toast.LENGTH_LONG);
-                            }
-                        });
+                Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
+                startActivityForResult(intent, 1);
             }
         });
 
     }
 
-    private void ImagePicker() {
-        Matisse.from(MainActivity.this)
-                .choose(MimeType.ofImage(), false)
-                .theme(R.style.Matisse_Dracula)
-                .countable(true)
-                .capture(true)
-                .captureStrategy(
-                        new CaptureStrategy(true, "com.zhihu.matisse.sample.fileprovider", "test"))
-                .maxSelectable(9)
-                .gridExpectedSize(
-                        getResources().getDimensionPixelSize(R.dimen.grid_expected_size))
-                .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
-                .thumbnailScale(0.85f)
-                .imageEngine(new GlideEngine())
-                .setOnSelectedListener((uriList, pathList) -> {
-                    Log.e("onSelected", "onSelected: pathList=" + pathList);
-                })
-                .showSingleMediaType(true)
-                .originalEnable(true)
-                .maxOriginalSize(10)
-                .autoHideToolbarOnSingleTap(true)
-                .setOnCheckedListener(isChecked -> {
-                    Log.e("isChecked", "onCheck: isChecked=" + isChecked);
-                })
-                .forResult(REQUEST_CODE_CHOOSE);
-    }
 
-    List<Uri> mSelected;
-    @Override //Receive the returned address
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE_CHOOSE && resultCode == RESULT_OK) {
-            mSelected = Matisse.obtainResult(data);
-            Log.d("Matisse", "mSelected: " + mSelected);
+//
+        } else {
+//
         }
-
     }
 }
